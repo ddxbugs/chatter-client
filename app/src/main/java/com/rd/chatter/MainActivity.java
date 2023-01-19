98bdf26c833a6c49c300db14493e849de963d5f5
ddxbugs <ddxbugs@proton.me>
2023-01-19 14:21:28 -0800

    app/MainActivity send intent message display activity

--- app/src/main/java/com/rd/chatter/MainActivity.java
+++ app/src/main/java/com/rd/chatter/MainActivity.java
@@ -2,13 +2,29 @@ package com.rd.chatter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.rd.chatter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final String EXTRA_MESSAGE="secret";
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.buttonMainSend.setOnClickListener(this::sendMessage);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = binding.textMainEdit.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
